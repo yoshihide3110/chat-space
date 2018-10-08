fa144c49b07a10b63806c1f819ab2ae7a67c8bd9
yoshihidesaito <groovers_yoshi@mac.com>
2018-10-08 15:11:04 +0900

    Add no-synnc

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -17,7 +17,6 @@ $(function(){
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
     var url= $(this).attr('action')
     $.ajax ({
