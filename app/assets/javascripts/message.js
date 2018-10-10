9036e462a9c5e5a7258afba049befb142e25255e
yoshihide3110 <40555395+yoshihide3110@users.noreply.github.com>
2018-10-10 20:40:30 +0900

    Revert "Add no-sync message1007"

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,41 +1,6 @@
 $(function(){
-  function buildHTML(message){
-    var html = `<div class="upper-message">
-                    <div class="top-message__user-name">
-                     ${message.user_name}
-                     </div>
-                    <div class="top-message__date">
-                    ${message.creted_at}
-                    </div>
-                    <div class="bottom-message">
-                    ${message.body}
-                    ${message.image}
-                  </div>
-                </div>`
-    return html
-  }
-
-  $('#new_message').on('submit', function(e){
+  $('messages#create').on('on submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url= $(this).attr('action')
-    $.ajax ({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false,
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.new_message').append(html);
-      $('form__message').val('');
-      $('form__submit').prop('disabled', false);
-      })
-    .fail(function(){
-      alert('error');
-    })
   })
 })
-
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,7 +1,4 @@
-json.body       @message.body
-json.user_name  @message.user.name
-json.group_id   @message.group.id
-json.date       @message.created_at
-json.image      @message.image.url
-json.id         @message.id
-json.date       @message.created_at.to_s(:datetime)
+json.body    @message.body
+json.user_id    @message.user_id
+json.group_id    @message.group_id
+
