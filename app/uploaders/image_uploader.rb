54206fd6b263bf53f623158f298b2936a68d59d5
yoshihidesaito <groovers_yoshi@mac.com>
2018-10-15 19:42:06 +0900

    Change this line

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
+  process resize_to_fit: [800,800]
   # Choose what kind of storage to use for this uploader:
   storage :fog
   # storage :fog
@@ -12,7 +12,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  process resize_to_fit: [800,800]
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
