8ccf156f0ff5900f34cf7cc86266b8c061847638
yoshihidesaito <groovers_yoshi@mac.com>
2018-10-15 21:12:40 +0900

    Edit index.js

--- app/assets/javascripts/channels/index.js
+++ app/assets/javascripts/channels/index.js
@@ -1,4 +1,4 @@
-$(function() {
+ $(function() {
   function buildHTML(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
